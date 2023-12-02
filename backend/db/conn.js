ab19f038d833f9d4e64d2e6f70000e46f4ae7beb
ZoeMesaros <zoe.mesaros@hotmail.com>
2023-12-02 18:45:24 +0100

    Server side and frontend set up. Database connection with Mongodb

---
+++ backend/db/conn.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var _db;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client
+      .connect()
+      .then((connection) => {
+        _db = connection.db("bakbutiken-db");
+        console.log("Successfully connected to MongoDB.");
+        callback();
+      })
+      .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+        callback(error);
+      });
+  },
+
+  getDb: function () {
+    return _db;
+  },
+};
---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config({ path: __dirname + "/config.env" });
+
+const port = process.env.PORT || 5000;
+
+// Import and initialize dbo before using it in routes
+const dbo = require("./db/conn");
+
+// Connect to MongoDB
+dbo.connectToServer((err) => {
+  if (err) {
+    console.error("Error connecting to MongoDB:", err);
+    return;
+  }
+
+  // Set up routes after successful connection
+  app.get("/products", async (req, res) => {
+    try {
+      const db_connect = dbo.getDb();
+      const products = await db_connect
+        .collection("products")
+        .find({})
+        .toArray();
+      res.json(products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+});
