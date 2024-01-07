2880b4493cbeb6246ce88016a00a0504d7ebadb5
Zoe <35260156+ZoeMesaros@users.noreply.github.com>
2024-01-07 13:34:40 +0100

    navbar fix

--- frontend/src/pages/Product/Product.js
+++ frontend/src/pages/Product/Product.js
@@ -32,91 +32,86 @@ const SingleProductPage = () => {
   };
 
   return (
-    <>
-      <div className="single-product-page">
-        <div className="product-info">
-          <div className="product-image">
-            <img src={product.imgSingle} alt={product.name} />
+    <div className="single-product-page">
+      <div className="product-info">
+        <div className="product-image">
+          <img src={product.imgSingle} alt={product.name} />
+        </div>
+        <div className="product-details">
+          <h2 className="product-name">{product.name}</h2>
+          <p className="product-price">
+            <span className={`item-price${product.onSale ? "-sale" : ""}`}>
+              {product.price} Kr
+            </span>
+            {product.onSale && (
+              <span className="sale-price">&nbsp; {product.salePrice} Kr</span>
+            )}
+          </p>
+          <div className="add-to-cart">
+            <button className="btn card-btn">Lägg till i kundvagnen</button>
           </div>
-          <div className="product-details">
-            <h2 className="product-name">{product.name}</h2>
-            <p className="product-price">
-              <span className={`item-price${product.onSale ? "-sale" : ""}`}>
-                {product.price} Kr
-              </span>
-              {product.onSale && (
-                <span className="sale-price">
-                  &nbsp; {product.salePrice} Kr
-                </span>
-              )}
-            </p>
-            <div className="add-to-cart">
-              <button className="btn card-btn">Lägg till i kundvagnen</button>
-            </div>
-            <div className="product-description">
-              <p>{product.desc}</p>
-            </div>
-            <div className="accordion" id="accordionFlushExample">
-              <div className="accordion-item">
-                <h2 className="accordion-header" id="flush-headingOne">
-                  <button
-                    className={`accordion-button ${
-                      specificationsOpen ? "" : "collapsed"
-                    }`}
-                    type="button"
-                    onClick={toggleSpecifications}
-                  >
-                    Specifikationer
-                  </button>
-                </h2>
-                <div
-                  id="flush-collapseOne"
-                  className={`accordion-collapse collapse ${
-                    specificationsOpen ? "show" : ""
+          <div className="product-description">
+            <p>{product.desc}</p>
+          </div>
+          <div className="accordion" id="accordionFlushExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="flush-headingOne">
+                <button
+                  className={`accordion-button ${
+                    specificationsOpen ? "" : "collapsed"
                   }`}
-                  aria-labelledby="flush-headingOne"
-                  data-bs-parent="#accordionFlushExample"
+                  type="button"
+                  onClick={toggleSpecifications}
                 >
-                  <div className="accordion-body">
-                    <ul>
-                      <li>{product.spec}</li>
-                      <li>{product.spec1}</li>
-                      <li>{product.spec2}</li>
-                    </ul>
-                  </div>
+                  Specifikationer
+                </button>
+              </h2>
+              <div
+                id="flush-collapseOne"
+                className={`accordion-collapse collapse ${
+                  specificationsOpen ? "show" : ""
+                }`}
+                aria-labelledby="flush-headingOne"
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div className="accordion-body">
+                  <ul>
+                    <li>{product.spec}</li>
+                    <li>{product.spec1}</li>
+                    <li>{product.spec2}</li>
+                  </ul>
                 </div>
               </div>
-              <div className="accordion-item">
-                <h2 className="accordion-header" id="flush-headingTwo">
-                  <button
-                    className={`accordion-button ${
-                      materialsOpen ? "" : "collapsed"
-                    }`}
-                    type="button"
-                    onClick={toggleMaterials}
-                  >
-                    Material
-                  </button>
-                </h2>
-                <div
-                  id="flush-collapseTwo"
-                  className={`accordion-collapse collapse ${
-                    materialsOpen ? "show" : ""
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="flush-headingTwo">
+                <button
+                  className={`accordion-button ${
+                    materialsOpen ? "" : "collapsed"
                   }`}
-                  aria-labelledby="flush-headingTwo"
-                  data-bs-parent="#accordionFlushExample"
+                  type="button"
+                  onClick={toggleMaterials}
                 >
-                  <div className="accordion-body">
-                    <p>{product.material}</p>
-                  </div>
+                  Material
+                </button>
+              </h2>
+              <div
+                id="flush-collapseTwo"
+                className={`accordion-collapse collapse ${
+                  materialsOpen ? "show" : ""
+                }`}
+                aria-labelledby="flush-headingTwo"
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div className="accordion-body">
+                  <p>{product.material}</p>
                 </div>
               </div>
-              {/* Add more accordion items as needed */}
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
