851ae868d1dbd8d097815f4494cbabd46ad52d61
Zoe <35260156+ZoeMesaros@users.noreply.github.com>
2024-01-07 10:11:40 +0100

    bootstrap & Cards styling fixed

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./navbar.scss";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../../assets/images/bakbutiken.png";
 
 const NavBar = () => {
@@ -35,97 +36,35 @@ const NavBar = () => {
           </div>
         </div>
         {/* Navigation bar */}
-        <nav
-          id="main-nav"
-          className="navbar navbar-expand-md navbar-light border"
-        >
-          <div className="collapse navbar-collapse" id="main_nav">
-            <ul className="navbar-nav mx-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to={"/"}>
-                  Hem
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/products"}>
-                  Produkter
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/products"}>
-                  Om oss
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/products"}>
-                  Kontakt
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        {/* Mobile Navigation Menu */}
-        <nav id="mobile-nav" className="navbar navbar-light navbar-1 white">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent15"
-            aria-controls="navbarSupportedContent15"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse"
-            id="navbarSupportedContent15"
-          >
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <div className="header-button-mobile">
-                  <button>
-                    <a
-                      href="https://github.com/mdbootstrap/bootstrap-material-design"
-                      target="_blank"
-                    >
-                      <i className="fas fa-user-alt m-1 me-md-2"></i>
-                      <p className="d-none d-md-block mb-0">Logga in</p>
-                    </a>
-                  </button>
-                  <button>
-                    <a
-                      href="https://github.com/mdbootstrap/bootstrap-material-design"
-                      target="_blank"
-                    >
-                      <i className="fas fa-shopping-cart m-1 me-md-2"></i>
-                      <p className="d-none d-md-block mb-0">Kundvagn</p>
-                    </a>
-                  </button>
-                </div>
-                <Link className="nav-link" to={"/"}>
-                  Hem <span className="sr-only">(current)</span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-item" to={"/products"}>
-                  {" "}
-                  Produkter
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Om oss
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Kontakt
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar expand="md" variant="light" className="border">
+          <Container>
+            <Navbar.Toggle aria-controls="navbarNav" />
+            <Navbar.Collapse id="navbarNav">
+              <Nav className="mx-auto">
+                <Nav.Item>
+                  <Link to={"/"} className="nav-link">
+                    Hem
+                  </Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link to={"/products"} className="nav-link">
+                    Produkter
+                  </Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link to={"/products"} className="nav-link">
+                    Om oss
+                  </Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link to={"/products"} className="nav-link">
+                    Kontakt
+                  </Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
     </>
   );
