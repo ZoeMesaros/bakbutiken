1fb1f66fc05dd21b858255a4a33eeaa800273bd8
Zoe <35260156+ZoeMesaros@users.noreply.github.com>
2024-01-15 18:09:19 +0100

    checkout forms & cart summary section

--- frontend/src/pages/Checkout/Checkout.js
+++ frontend/src/pages/Checkout/Checkout.js
@@ -59,162 +59,65 @@ const CheckoutPage = ({ cart }) => {
 
   return (
     <div className="checkout-page">
+      <h3>Kassan</h3>
       {cart.length > 0 ? (
-        <div className="row">
-          <div className="col-md-8 address">
-            <div className="checkout-title">
-              <div className="row">
-                <div className="col">
-                  <h3>Kassan</h3>
-                </div>
-                <div className="col align-self-center text-right text-muted"></div>
-              </div>
-            </div>
-            <h5>Uppgifter</h5>
-            <div className="checkout-form-div">
-              <form id="checkout-form" onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-input-row">
-                  <div>
-                    {/* First name label */}
-                    <label htmlFor="form-firstname"></label>
-                    {errors.first_name && (
-                      <span className="form-error">Ange ett namn</span>
-                    )}
-                    <input
-                      {...register("first_name", { required: true })}
-                      type="text"
-                      id="form-firstname"
-                      placeholder="Förnamn"
-                    />
-                  </div>
-                  <div>
-                    {/* Last name label */}
-                    <label htmlFor="form-lastname"></label>
-                    {errors.last_name && (
-                      <span className="form-error">Ange ett efternamn</span>
-                    )}
-                    <input
-                      {...register("last_name", { required: true })}
-                      type="text"
-                      id="form-lastname"
-                      placeholder="Efternamn"
-                    />
-                  </div>
-                  <div>
-                    {/* Address label*/}
-                    <label htmlFor="form-address"></label>
-                    {errors.address && (
-                      <span className="form-error">Ange en adress</span>
-                    )}
-                    <input
-                      {...register("address", { required: true })}
-                      type="text"
-                      id="input-address"
-                      placeholder="Adress"
-                    />
-                  </div>
-                  <div>
-                    {/* Postal label*/}
-                    <label htmlFor="form-address"></label>
-                    {errors.postal && (
-                      <span className="form-error">Ange ett postnummer</span>
-                    )}
-                    <input
-                      {...register("postal", { required: true })}
-                      type="text"
-                      id="input-postal"
-                      placeholder="Postnummer"
-                    />
-                  </div>
-                  <div>
-                    {/* Area */}
-                    <label htmlFor="form-address"></label>
-                    {errors.area && (
-                      <span className="form-error">Ange en postort</span>
-                    )}
-                    <input
-                      {...register("area", { required: true })}
-                      type="text"
-                      id="input-area"
-                      placeholder="Postort"
-                    />
-                  </div>
-                </div>
-                <div className="form-input-row">
-                  <div>
-                    {/* E-email */}
-                    <label htmlFor="form-email"></label>{" "}
-                    {errors.email && (
-                      <span className="form-error">Ange en giltig e-post</span>
-                    )}
-                    <input
-                      {...register("email", {
-                        required: true,
-                        pattern: {
-                          value:
-                            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                          message: "Ogiltig e-postadress",
-                        },
-                      })}
-                      type="text"
-                      id="form-email"
-                      placeholder="E-post"
-                    />
-                  </div>
-                  <div>
-                    {/* Mobile */}
-                    <label htmlFor="form-mobile"></label>
-                    {errors.mobile && (
-                      <span className="form-error">
-                        Ange ett giltigt telefonnummer
-                      </span>
-                    )}
-                    <input
-                      {...register("mobile", {
-                        required: false,
-                        pattern: {
-                          value: /^[0-9\s+-]+$/,
-                        },
-                      })}
-                      type="tel"
-                      id="form-mobile"
-                      placeholder="Mobilnummer"
-                    />
-                  </div>
-                  <div>
-                    {/* Phone */}
-                    <label htmlFor="form-phone"></label>
-                    {errors.phone && (
-                      <span className="form-error">
-                        Ange ett giltigt telefonnummer
-                      </span>
-                    )}
-                    <input
-                      {...register("phone", {
-                        required: false,
-                        pattern: {
-                          value: /^[0-9\s+-]+$/,
-                        },
-                      })}
-                      type="tel"
-                      id="form-phone"
-                      placeholder="Telefonnummer"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <button type="submit" form="checkout-form">
-                    Skicka
-                  </button>
-                </div>
-              </form>
+        <div className="col summary border-left border-right">
+          <div className="row border-top border-bottom">
+            <div className="row main align-items-center">
+              <div className="col-2"></div>
+              <div className="col">Artikel</div>
+              <div className="col">Antal</div>
+              <div className="col">À-pris </div>
+              <div className="col">Summa</div>
+              <div className="col-2"></div>
             </div>
           </div>
-          <div className="col-md-4 summary border-left border-right">
-            <div className="cart-summary">
-              <h5>Sammanfattning</h5>
+          <h5>Sammanfattning</h5>
+          <div className="cart-summary">
+            <div className="checkout-cart">
+              {cart.map((cartItem, index) => (
+                <div className="row border-top border-bottom" key={index}>
+                  <div className="row main align-items-center">
+                    <div className="col-2">
+                      <img
+                        className="img-fluid"
+                        src={cartItem.img}
+                        alt={cartItem.name}
+                      />
+                    </div>
+                    <div className="col">
+                      <Link to={`/products/${cartItem.slug}`}>
+                        <div className="row text-muted">{cartItem.name}</div>
+                      </Link>
+                    </div>
+                    <div className="col">{cartItem.quantity}</div>
+                    <div className="col">
+                      <div className="price-container">
+                        <span
+                          className={`item-price${
+                            cartItem.onSale ? "-sale" : ""
+                          }`}
+                        >
+                          {cartItem.price} Kr
+                        </span>
+                        {cartItem.onSale && (
+                          <span className="sale-price">
+                            {cartItem.salePrice} Kr
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                    <div className="col">
+                      {cartItem.quantity * cartItem.price} Kr
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="summary-section">
               <p>
-                Antal varor:&nbsp;<strong>{calculateTotalQuantity()}</strong>
+                Antal varor:&nbsp;
+                <strong>{calculateTotalQuantity()}</strong>
               </p>
               <p>
                 Summa varor:&nbsp;<strong>{calculateTotalSum()} Kr</strong>
@@ -234,6 +137,140 @@ const CheckoutPage = ({ cart }) => {
                 <Link to={"/checkout"}>Till betalning</Link>
               </button>
             </div>
+          </div>
+          <h5>Dina uppgifter</h5>
+          <div className="order-info">
+            <form id="checkout-form" onSubmit={handleSubmit(onSubmit)}>
+              <div className="col checkout-form-div">
+                <div className="form-input-row">
+                  <div>
+                    {/* First name label */}
+                    {errors.first_name && (
+                      <span className="form-error">Ange ett namn</span>
+                    )}
+
+                    <input
+                      {...register("first_name", { required: true })}
+                      type="text"
+                      id="form-firstname"
+                      placeholder="Förnamn"
+                    />
+                  </div>
+                  <div>
+                    {/* Last name label */}
+                    {errors.last_name && (
+                      <span className="form-error">Ange ett efternamn</span>
+                    )}
+                    <input
+                      {...register("last_name", { required: true })}
+                      type="text"
+                      id="form-lastname"
+                      placeholder="Efternamn"
+                    />
+                  </div>
+                  <div>
+                    {/* Address label*/}
+                    {errors.address && (
+                      <span className="form-error">Ange en adress</span>
+                    )}
+                    <input
+                      {...register("address", { required: true })}
+                      type="text"
+                      id="input-address"
+                      placeholder="Adress"
+                    />
+                  </div>
+                  <div>
+                    {/* Postal label*/}
+                    {errors.postal && (
+                      <span className="form-error">Ange ett postnummer</span>
+                    )}
+                    <input
+                      {...register("postal", { required: true })}
+                      type="text"
+                      id="input-postal"
+                      placeholder="Postnummer"
+                    />
+                  </div>
+                  <div>
+                    {/* Area */}
+                    {errors.area && (
+                      <span className="form-error">Ange en postort</span>
+                    )}
+                    <input
+                      {...register("area", { required: true })}
+                      type="text"
+                      id="input-area"
+                      placeholder="Postort"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col checkout-form-div">
+                <div>
+                  {/* E-email */}
+                  {errors.email && (
+                    <span className="form-error">Ange en giltig e-post</span>
+                  )}
+                  <input
+                    {...register("email", {
+                      required: true,
+                      pattern: {
+                        value:
+                          /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                        message: "Ogiltig e-postadress",
+                      },
+                    })}
+                    type="text"
+                    id="form-email"
+                    placeholder="E-post"
+                  />
+                </div>
+                <div>
+                  {/* Mobile */}
+                  {errors.mobile && (
+                    <span className="form-error">
+                      Ange ett giltigt telefonnummer
+                    </span>
+                  )}
+                  <input
+                    {...register("mobile", {
+                      required: false,
+                      pattern: {
+                        value: /^[0-9\s+-]+$/,
+                      },
+                    })}
+                    type="tel"
+                    id="form-mobile"
+                    placeholder="Mobilnummer"
+                  />
+                </div>
+                <div>
+                  {/* Phone */}
+                  {errors.phone && (
+                    <span className="form-error">
+                      Ange ett giltigt telefonnummer
+                    </span>
+                  )}
+                  <input
+                    {...register("phone", {
+                      required: false,
+                      pattern: {
+                        value: /^[0-9\s+-]+$/,
+                      },
+                    })}
+                    type="tel"
+                    id="form-phone"
+                    placeholder="Telefonnummer"
+                  />
+                </div>
+              </div>
+              {/*        <div>
+                <button type="submit" form="checkout-form">
+                  Skicka
+                </button>
+              </div> */}
+            </form>
           </div>
         </div>
       ) : (
