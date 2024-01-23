2dbd0767c3320c6e3247550f19ec3394d2f67f03
Zoe <35260156+ZoeMesaros@users.noreply.github.com>
2024-01-23 15:34:34 +0100

    stylig fix margins, background color

--- frontend/src/pages/Product/Product.js
+++ frontend/src/pages/Product/Product.js
@@ -54,107 +54,115 @@ const SingleProductPage = ({ cart, addToCart, removeFromCart }) => {
 
   return (
     <div className="single-product-page">
-      <Link to={"/products"}>
-        <i className="fa-solid fa-arrow-left-long"></i>&nbsp;Gå tillbaka
-      </Link>
-      <div className="product-info">
-        <div className="product-image">
-          <img src={product.imgSingle} alt={product.name} />
-        </div>
-        <div className="product-details">
-          <h2 className="product-name">{product.name}</h2>
-          <p className="product-price">
-            <span className={`item-price${product.onSale ? "-sale" : ""}`}>
-              {product.price} Kr
-            </span>
-            {product.onSale && (
-              <span className="sale-price">&nbsp; {product.salePrice} Kr</span>
-            )}
-          </p>
-          <div className="add-to-cart">
-            <div
-              className={`add-to-cart-container${
-                existingCartItem ? "-added" : ""
-              }`}
-            >
-              <button className="btn card-btn" onClick={AddToCart}>
-                Lägg till i kundvagnen
-              </button>
-              {existingCartItem && existingCartItem.quantity >= 1 && (
-                <div className="qty-container">
-                  <button className="btn" onClick={RemoveFromCart}>
-                    -
-                  </button>
-                  <span>{existingCartItem.quantity}</span>
-                  <button
-                    className={`btn${
-                      existingCartItem.quantity >= product.inStock ? "-max" : ""
-                    }`}
-                    onClick={AddToCart}
-                  >
-                    +
-                  </button>
-                </div>
-              )}
+      <div className="container">
+        <div className="row">
+          <Link to={"/products"}>
+            <i className="fa-solid fa-arrow-left-long"></i>&nbsp;Gå tillbaka
+          </Link>
+          <div className="product-info">
+            <div className="product-image">
+              <img src={product.imgSingle} alt={product.name} />
             </div>
-          </div>
-          <br></br>
-          <p className="in-stock">Antal varor i lager: {product.inStock}</p>
-          <div className="product-description">
-            <p>{product.desc}</p>
-          </div>
-          <div className="accordion" id="accordionFlushExample">
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="flush-headingOne">
-                <button
-                  className={`accordion-button ${
-                    specificationsOpen ? "" : "collapsed"
+            <div className="product-details">
+              <h2 className="product-name">{product.name}</h2>
+              <p className="product-price">
+                <span className={`item-price${product.onSale ? "-sale" : ""}`}>
+                  {product.price} Kr
+                </span>
+                {product.onSale && (
+                  <span className="sale-price">
+                    &nbsp; {product.salePrice} Kr
+                  </span>
+                )}
+              </p>
+              <div className="add-to-cart">
+                <div
+                  className={`add-to-cart-container${
+                    existingCartItem ? "-added" : ""
                   }`}
-                  type="button"
-                  onClick={toggleSpecifications}
                 >
-                  Specifikationer
-                </button>
-              </h2>
-              <div
-                id="flush-collapseOne"
-                className={`accordion-collapse collapse ${
-                  specificationsOpen ? "show" : ""
-                }`}
-                aria-labelledby="flush-headingOne"
-                data-bs-parent="#accordionFlushExample"
-              >
-                <div className="accordion-body">
-                  <ul>
-                    <li>{product.spec}</li>
-                    <li>{product.spec1}</li>
-                    <li>{product.spec2}</li>
-                  </ul>
+                  <button className="btn card-btn" onClick={AddToCart}>
+                    Lägg till i kundvagnen
+                  </button>
+                  {existingCartItem && existingCartItem.quantity >= 1 && (
+                    <div className="qty-container">
+                      <button className="btn" onClick={RemoveFromCart}>
+                        -
+                      </button>
+                      <span>{existingCartItem.quantity}</span>
+                      <button
+                        className={`btn${
+                          existingCartItem.quantity >= product.inStock
+                            ? "-max"
+                            : ""
+                        }`}
+                        onClick={AddToCart}
+                      >
+                        +
+                      </button>
+                    </div>
+                  )}
                 </div>
               </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="flush-headingTwo">
-                <button
-                  className={`accordion-button ${
-                    materialsOpen ? "" : "collapsed"
-                  }`}
-                  type="button"
-                  onClick={toggleMaterials}
-                >
-                  Material
-                </button>
-              </h2>
-              <div
-                id="flush-collapseTwo"
-                className={`accordion-collapse collapse ${
-                  materialsOpen ? "show" : ""
-                }`}
-                aria-labelledby="flush-headingTwo"
-                data-bs-parent="#accordionFlushExample"
-              >
-                <div className="accordion-body">
-                  <p>{product.material}</p>
+              <br></br>
+              <p className="in-stock">Antal varor i lager: {product.inStock}</p>
+              <div className="product-description">
+                <p>{product.desc}</p>
+              </div>
+              <div className="accordion" id="accordionFlushExample">
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="flush-headingOne">
+                    <button
+                      className={`accordion-button ${
+                        specificationsOpen ? "" : "collapsed"
+                      }`}
+                      type="button"
+                      onClick={toggleSpecifications}
+                    >
+                      Specifikationer
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseOne"
+                    className={`accordion-collapse collapse ${
+                      specificationsOpen ? "show" : ""
+                    }`}
+                    aria-labelledby="flush-headingOne"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      <ul>
+                        <li>{product.spec}</li>
+                        <li>{product.spec1}</li>
+                        <li>{product.spec2}</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="flush-headingTwo">
+                    <button
+                      className={`accordion-button ${
+                        materialsOpen ? "" : "collapsed"
+                      }`}
+                      type="button"
+                      onClick={toggleMaterials}
+                    >
+                      Material
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseTwo"
+                    className={`accordion-collapse collapse ${
+                      materialsOpen ? "show" : ""
+                    }`}
+                    aria-labelledby="flush-headingTwo"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      <p>{product.material}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
