d48feab887e0685b20eb55a3d877bc0790f2f4ff
Zoe <35260156+ZoeMesaros@users.noreply.github.com>
2024-01-25 15:56:25 +0100

    footer in progress

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,105 +1,135 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import logo from "../../assets/images/bakbutiken.png";
 import "./footer.scss";
 
 // Footer component
 const Footer = () => {
+  const [isRegistered, setRegistered] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Form data submitted:", data);
+    setRegistered(true);
+  };
   return (
     <footer>
-      <div class="container py-5">
-        <div class="row py-4">
-          <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-            <img src="img/logo.png" alt="" width="180" class="mb-3" />
-            <p class="font-italic text-muted">
+      <div className="container py-5">
+        <div className="row py-4">
+          <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
+            <img src={logo} alt="Bakbutiken" width="180" className="mb-2" />
+            <p>
               Välkommen till Bakbutiken - din nya plats för högkvalitativa
               bakningstillbehör! Utforska vårt utvalda sortiment för fest och
               vardagsbaking. Välkommen in och upplev glädjen med att baka hos
               Bakbutiken!
             </p>
-
-            <ul class="list-inline mt-4">
-              <li class="list-inline-item">
-                <a href="#" target="_blank" title="twitter">
-                  <i class="fa fa-twitter"></i>
+            <ul className="footer-social p-0">
+              <li>
+                <a className="social-icon">
+                  <i className="fab fa-twitter"></i>
                 </a>
               </li>
-              <li class="list-inline-item">
-                <a href="#" target="_blank" title="facebook">
-                  <i class="fa fa-facebook"></i>
+              <li>
+                <a className="social-icon">
+                  <i className="fab fa-facebook"></i>
                 </a>
               </li>
-              <li class="list-inline-item">
-                <a href="#" target="_blank" title="instagram">
-                  <i class="fa fa-instagram"></i>
+              <li>
+                <a className="social-icon">
+                  <i className="fab fa-instagram"></i>
                 </a>
               </li>
-              <li class="list-inline-item">
-                <a href="#" target="_blank" title="pinterest">
-                  <i class="fa fa-pinterest"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="#" target="_blank" title="vimeo">
-                  <i class="fa fa-vimeo"></i>
+              <li>
+                <a className="social-icon">
+                  <i className="fab fa-linkedin"></i>
                 </a>
               </li>
             </ul>
           </div>
-          <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-            <h6 class="text-uppercase font-weight-bold mb-4">Produkter</h6>
-            <ul class="list-unstyled mb-0">
-              <li class="mb-2">
+          <div className="col-lg-2 col-md-6 mb-4 mb-lg-0">
+            <h6 className="text-uppercase font-weight-bold mb-4">Produkter</h6>
+            <ul className="list-unstyled mb-0">
+              <li className="mb-2">
                 <Link to={"/products/pans"}>Bakformar</Link>
               </li>
-              <li class="mb-2">
+              <li className="mb-2">
                 <Link to={"/products/utensils"}>Verktyg</Link>
               </li>
-              <li class="mb-2">
+              <li className="mb-2">
                 <Link to={"/products/bowls"}>Skålar</Link>
               </li>
-              <li class="mb-2">
+              <li className="mb-2">
                 <Link to={"/products/decorations"}>Dekorationer</Link>
               </li>
             </ul>
           </div>
-          <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-            <h6 class="text-uppercase font-weight-bold mb-4">Länkar</h6>
-            <ul class="list-unstyled mb-0">
-              <li class="mb-2">
+          <div className="col-lg-2 col-md-6 mb-4 mb-lg-0">
+            <h6 className="text-uppercase font-weight-bold mb-4">Länkar</h6>
+            <ul className="list-unstyled mb-0">
+              <li className="mb-2">
                 <Link to={"/"}>Hem</Link>
               </li>
-              <li class="mb-2">
+              <li className="mb-2">
                 <Link to={"/products"}>Produkter</Link>
               </li>
-              <li class="mb-2">
+              <li className="mb-2">
                 <Link to={"/about"}>Om oss</Link>
               </li>
-              <li class="mb-2">
+              <li className="mb-2">
                 <Link to={"/contact"}>Kontakt</Link>
               </li>
             </ul>
           </div>
-          <div class="col-lg-4 col-md-6 mb-lg-0">
-            <h6 class="text-uppercase font-weight-bold mb-4">Newsletter</h6>
-            <p class="text-muted mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-              itaque temporibus.
+          <div className="col-lg-4 col-md-6 mb-lg-0">
+            <h6 className="text-uppercase font-weight-bold mb-4">Nyhetsbrev</h6>
+            <p className="text-muted mb-4">
+              Prenumerera på vårt nyhetsbrev för att ta del av de senaste
+              erbjudandena och spännande uppdateringar
             </p>
-            <div class="p-1 rounded border">
-              <div class="input-group">
-                <input
-                  type="email"
-                  placeholder="Enter your email address"
-                  aria-describedby="button-addon1"
-                  class="form-control border-0 shadow-0"
-                />
-                <div class="input-group-append">
-                  <button id="button-addon1" type="submit" class="btn btn-link">
-                    <i class="fa fa-paper-plane"></i>
-                  </button>
+            {isRegistered ? (
+              <p className="text-muted mb-4">
+                Du är nu registrerad till vårt nyhetsbrev
+              </p>
+            ) : (
+              <form
+                className="subscribe-form"
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <div className="form-group d-flex">
+                  <input
+                    {...register("email", {
+                      required: true,
+                      pattern: {
+                        value:
+                          /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                        message: "Ogiltig e-postadress",
+                      },
+                    })}
+                    type="text"
+                    className="form-control rounded-left"
+                    placeholder="Din e-postadress"
+                    name="email"
+                  />
+                  <input
+                    type="submit"
+                    value="Prenumerera"
+                    className="form-control submit px-3 rounded-right"
+                  />
                 </div>
-              </div>
-            </div>
+                {errors.email && (
+                  <span className="form-error">
+                    &nbsp;Ange en giltig e-post adress
+                  </span>
+                )}
+              </form>
+            )}
           </div>
         </div>
       </div>
